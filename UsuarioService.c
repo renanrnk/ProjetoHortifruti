b880afb1b6a1276bd64dda3aa574b8d07097e8a5
Renan Nogueira <renan.dev012@gmail.com>
2024-10-09 11:49:02 -0300

    Melhoria CadastrarUsuario e implementacao LoginUsuario

--- UsuarioService.c
+++ UsuarioService.c
@@ -5,45 +5,93 @@
 
 #define SENHA_ADMIN "admin123"
 
-// Definição de um usuário
+
 typedef struct {
-    char nome[50];
+    char email[50];
     char senha[50];
     int isAdmin;
 } Usuario;
 
 void CadastrarUsuario()
 {
+    
     char senhaAdmin[50];
     Usuario usuario;
     FILE *arquivo;
+    char emailExistente[50], senhaExistente[50];
+    int isAdminExistente;
+    int emailJaExiste = 0;
 
-    // Pede o nome e a senha do usuário
-    printf("Digite o nome do usuario: ");
-    scanf("%s", usuario.nome);
+    
+    printf("Digite o email do usuario: ");
+    scanf("%s", usuario.email);
+
+    arquivo = fopen("usuarios.txt", "r");
+        if (arquivo != NULL) {
+        while (fscanf(arquivo, "%s %s %d", emailExistente, senhaExistente, &isAdminExistente) != EOF) {
+            if (strcmp(emailExistente, usuario.email) == 0) {
+                printf("Erro: Email ja cadastrado!\n");
+                emailJaExiste = 1;
+                break;
+            }
+        }
+        fclose(arquivo);
+    }
+    if (emailJaExiste) {
+        return; 
+    }
     printf("Digite a senha do usuario: ");
     scanf("%s", usuario.senha);
-    printf("Digite a senha de administrador caso voce seja admin ou pressione digite 0 para ignorar: ");
+    printf("Digite a senha de administrador caso voce seja admin ou digite 0 para ignorar: ");
     scanf("%s", senhaAdmin);
 
+
     if (strcmp(senhaAdmin, SENHA_ADMIN) == 0) {
-        usuario.isAdmin = 1; // Se a senha for correta, torna-se admin
+        usuario.isAdmin = 1; 
         printf("Usuario cadastrado como administrador!\n");
     } else {
-        usuario.isAdmin = 0; // Caso contrário, é um usuário comum
+        usuario.isAdmin = 0; 
         printf("Usuario cadastrado como comum.\n");
     }
 
-    // Abre o arquivo em modo de anexação para adicionar novos usuários
+    
     arquivo = fopen("usuarios.txt", "a");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo!\n");
         return;
     }
 
-    // Escreve os dados do usuário no arquivo, com isAdmin formatado corretamente
-    fprintf(arquivo, "%s %s %d\n", usuario.nome, usuario.senha, usuario.isAdmin);
+    
+    fprintf(arquivo, "%s %s %d\n", usuario.email, usuario.senha, usuario.isAdmin);
     fclose(arquivo);
 
     printf("Usuario cadastrado com sucesso!\n");
+}
+
+void LoginUsuario()
+{
+    Usuario usuario;
+    FILE *arquivo;
+    char emailExistente[50], senhaExistente[50];
+    int verificaAdmin = 0;
+
+    printf("Digite seu email: ");
+    scanf("%s", usuario.email);
+    printf("Digite sua senha: ");
+    scanf("%s", usuario.senha);
+
+    arquivo = fopen("usuarios.txt", "r");
+    if (arquivo != NULL) {
+        while (fscanf(arquivo, "%s %s %d", emailExistente, senhaExistente, &verificaAdmin) != EOF) {
+            if (strcmp(emailExistente, usuario.email) == 0 && strcmp(senhaExistente, usuario.senha) == 0) {
+                printf("Logado com Sucesso!\n");
+                fclose(arquivo);
+                return; 
+            }
+        }
+        printf("Email ou senha incorretos.\n");
+        fclose(arquivo);
+    } else {
+        printf("Erro ao abrir o arquivo de usuarios.\n");
+    }
 }
--- main.c
+++ main.c
@@ -1,24 +1,29 @@
 #include "UsuarioService.c"
 
+
 int main() {
     int opcao;
 
-    while (1) // Loop infinito até que o usuário decida parar
+    while (1) 
     {
-        // Exibe o menu e pede a entrada do usuário
+        
         printf("\n1. Cadastrar Usuario\n");
-        printf("2. Sair\n");
+        printf("2. Fazer Login\n");
+        printf("3. Sair\n");
         printf("Escolha uma opcao: ");
         
-        scanf("%d", &opcao); // Lê a opção escolhida pelo usuário
+        scanf("%d", &opcao); 
 
         switch (opcao)
         {
             case 1:
-                CadastrarUsuario(); // Chama a função de cadastro
+                CadastrarUsuario(); 
                 break;
             case 2:
-                return 0;    
+                LoginUsuario(); 
+                break;
+            case 3:
+                return 0;
             default:
                 printf("Opcao invalida!\n");
         }
@@ -26,3 +31,5 @@ int main() {
 
     return 0;
 }
+
+
